9195d41e2f3bd71ceb829f6021e8e4de227da0f5
Wassim Rekik <rekikwassim23@gmail.com>
2018-05-29 15:40:06 +0100

    Initial Commit

---
+++ SideMenu_iOSTests/SideMenu_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SideMenu_iOSTests.swift
+//  SideMenu_iOSTests
+//
+//  Created by Wassim Rekik on 29/05/2018.
+//  Copyright © 2018 Wassim Rekik. All rights reserved.
+//
+
+import XCTest
+@testable import SideMenu_iOS
+
+class SideMenu_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
