0b252e1235b882ba5ab840d841ca01f592bfa2a5
Wassim Rekik <rekikwassim23@gmail.com>
2018-05-31 03:11:10 +0100

    First commit

---
+++ SideMenu_iOS/CostumAlertViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CostumAlertViewController.swift
+//  SideMenu_iOS
+//
+//  Created by Wassim Rekik on 31/05/2018.
+//  Copyright © 2018 Wassim Rekik. All rights reserved.
+//
+
+import UIKit
+
+class CostumAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
